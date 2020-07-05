3a1c9fde25cf7821734e32f443f434c42dcf9394
kazuki okazaki <prunzfrfx@gmail.com>
2020-07-05 21:46:07 +0900

    Transition to reslut page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
@@ -201,6 +202,18 @@ class _InputPageState extends State<InputPage> {
                     ),
                 ),
               ],
+            ),
+          ),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ResultPage()));
+            },
+            child: Container(
+              child: Text('CALCLATE'),
+              color: Colors.pink,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80.0,
             ),
           ),
         ],
